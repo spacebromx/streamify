9b798bea049d68e0483e21ab0bd075b0b9090983
César Aparicio <clark012389@hotmail.com>
2020-04-20 21:35:28 -0500

    Files Base Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Search from './components/search/Search'
+import HomeWidget from './components/home-widget/HomeWidget'
+import TopList from './components/topList/TopList'
+import Playbar from './components/playbar/Playbar'
+
 
 const App = () => {
-  return <div>
-    hello world
-  </div>
+  return (
+    <div>
+      <Header />
+      <Search />
+      <HomeWidget />
+      <HomeWidget />
+      <TopList />
+      <Playbar />
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+        <img src="" alt="logo" />
+        <p>We have over 2,000 songs, ready for you. Search, play and enjoy. Start typing and search through our catalog</p>
+    </header>
+}
+
+export default Header;
