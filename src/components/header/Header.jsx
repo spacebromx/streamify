df38f00a44ad23f036f803d9b0ef52079d9ecf50
César Aparicio <clark012389@hotmail.com>
2020-04-21 20:55:32 -0500

    Added Fix PR for Header component and styles in general

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './_Header.scss'
-import imageLogo from 'assets/compact-disc-solid.png'
+// import imageLogo from 'assets/compact-disc-solid.png'
 
 const Header = () => {
     return <header className="header">
-        <div className="header__logo">
-            <img className="header__logo__icon" src={imageLogo} alt="logo streamify" />
-            <span className="header__logo__brand">Streamify</span>
+        <div className="logo">
+            {/* <img className="logo__icon" src={imageLogo} alt="logo streamify" /> */}
+            <span className="logo__brand">Streamify</span>
         </div>
         <p className="header__legend">We have over 2,000 songs, ready for you. Search, play and enjoy. <br /> Start typing and search through our catalog</p>
     </header>
