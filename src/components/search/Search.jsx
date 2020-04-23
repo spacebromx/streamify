3dd6a28da3978873d67909d646f0c280c68c4af9
César Aparicio <clark012389@hotmail.com>
2020-04-23 07:41:03 -0500

    Added the component search,  _Utils.css and image for icon search (#6)
    * Added the component search,  _Utils.css and image for icon search
    
    * Added FIX PR to Search Component, implementation of font awesome and delete icon asset
    
    * Added focus styling and refactored naming
    
    Co-authored-by: Alan Christopher Alamillo Medina <realconect@gmail.com>

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { Component } from "react";
+import "./_Search.scss";
 
-const Search = () => {
-    return <div>
-        <input type="text" placeholder="Search here..."/>
-    </div>
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      query: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <div className="search__container">
+          <input
+            className="search__input"
+            type="text"
+            placeholder="Search here..."
+            value={this.state.query}
+            onChange={this.handleChange}
+          />
+          <span className="search__icon">
+            <i class="fas fa-search search__image"></i>
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Search;
