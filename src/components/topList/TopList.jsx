e7ee9027c44a71eb64425b4069c3e2374056574b
Alan Christopher Alamillo Medina <realconect@gmail.com>
2020-04-23 12:55:25 -0500

    Fixed linting issues

--- src/components/topList/TopList.jsx
+++ src/components/topList/TopList.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Song from '../song/Song';
 
 const TopList = () => {
-    return <div>
-        <h2>Top J-Pop</h2>
-        <Song />
-        <Song />
-        <Song />
-    </div>
+  return <div>
+    <h2>Top J-Pop</h2>
+    <Song />
+    <Song />
+    <Song />
+  </div>
 }
  
 export default TopList;
