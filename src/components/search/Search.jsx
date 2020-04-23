6217d9680ac2ae6aff6671ad771cef5cb043ebf7
César Aparicio <clark012389@hotmail.com>
2020-04-22 22:46:29 -0500

    Added the component search,  _Utils.css and image for icon search

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { Component } from "react";
+import "./_Search.scss";
+import searchIcon from "assets/search-solid.png";
 
-const Search = () => {
-    return <div>
-        <input type="text" placeholder="Search here..."/>
-    </div>
+class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      query: " ",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      query: e.target.value
+    });
+  }
+
+  render() {
+    return <div className="search">
+             <div className="search__container">
+               <input
+                 className="search__input"
+                 type="text"
+                 placeholder="Search here..."
+                 value={this.state.query}
+                 onChange={this.handleChange}
+               />
+               <span className="search__icon">
+                 <img className="search__image" src={searchIcon} alt="search icon" />
+               </span>
+             </div>
+           </div>
+  }
 }
- 
+
 export default Search;
