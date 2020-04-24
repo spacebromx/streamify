78adb5a4dd3270ad5c10db619c1bfc0694d5e4ae
Alan Medina <realconect@gmail.com>
2020-04-24 14:11:56 -0500

    Added Express server (#11)
    * Added express server
    
    * Fixed type

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+if(process.env.NODE_ENV === 'development') {
+  app.use(cors())
+}
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`)
+})
+
