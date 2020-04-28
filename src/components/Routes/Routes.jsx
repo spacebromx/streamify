2f6d3f9378e15a05953c3ce617704ba4c05fb03e
Alan Medina <realconect@gmail.com>
2020-04-28 12:19:11 -0500

    Added react query (#15)
    * Added react query
    
    * Added react query

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,32 +1,51 @@
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
-
+import Modal from '../modals/Modal'
 import App from '../../App'
-import Album from 'components/pages/album/Album'
-import Artist from 'components/pages/artist/Artist'
+import {ReactQueryConfigProvider} from 'react-query'
+
+const Artist = lazy(() => import('components/pages/artist/Artist'))
+const Album = lazy(() => import('components/pages/album/Album'))
+
+const queryConfig = {
+  suspense: true
+}
+
+const loadingModal = <Modal>
+  <div className="fa-3x">
+    <i className="fas fa-circle-notch fa-spin"/>
+    <p className="header__legend">Loading</p>
+  </div>
+</Modal>
 
 const Routes = () =>
   <Router>
-    <Switch>
-      <Route exact path="/">
-        <App />
-      </Route>
-      <Route path="/artist/:artist">
-        <Artist />
-      </Route>
-      <Route path="/artist">
-        <Redirect to="/"/>
-      </Route>
-      <Route path="/album/:album">
-        <Album />
-      </Route>
-      <Route path="/album">
-        <Redirect to="/"/>
-      </Route>
-      <Route path="*">
-        <div>Error 404</div>
-      </Route>
-    </Switch>
+    <ReactQueryConfigProvider config={queryConfig}>
+      <Switch>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route path="/artist/:artist">
+          <Suspense fallback={loadingModal}>
+            <Artist />
+          </Suspense>
+        </Route>
+        <Route path="/artist">
+          <Redirect to="/"/>
+        </Route>
+        <Route path="/album/:album">
+          <Suspense fallback={loadingModal}>
+            <Album />
+          </Suspense>
+        </Route>
+        <Route path="/album">
+          <Redirect to="/"/>
+        </Route>
+        <Route path="*">
+          <div>Error 404</div>
+        </Route>
+      </Switch>
+    </ReactQueryConfigProvider>
   </Router>
 
 export default Routes
--- src/components/pages/artist/Artist.jsx
+++ src/components/pages/artist/Artist.jsx
@@ -1,5 +1,6 @@
-import React, { Fragment, useEffect, useState } from 'react'
+import React from 'react'
 import { Link, useParams } from 'react-router-dom'
+import {useQuery} from 'react-query'
 import {API_URL} from 'constants'
 import {fetchInfo} from 'utils'
 import PageLayout from 'components/pages/page-layout/PageLayout'
@@ -7,48 +8,36 @@ import Playbar from 'components/common/playbar/Playbar'
 import Tracklist from 'components/common/tracklist/Tracklist'
 
 const Artist = () => {
-  const [artistInfo, setArtistInfo] = useState({})
-  const [tracklist, setTracklist] = useState([])
   const {artist} = useParams()
-
-  useEffect(() => {
-    (async () => {
-      setArtistInfo(await fetchInfo(`${API_URL}/artist/${artist}`))
-      setTracklist(await fetchInfo(`${API_URL}/artist/${artist}/top?limit=15`))
-    })();
-
-  }, [])
-
+  const fetchData = async () => await fetchInfo(`${API_URL}/artist/${artist}`)
+  const fetchTracks = async () => await fetchInfo(`${API_URL}/artist/${artist}/top?limit=15`)
+  const {data: artistInfo} = useQuery('artist', fetchData)
+  const {data: tracklist} = useQuery('tracklist', fetchTracks)
 
   return <main className="App">
-    {artistInfo?.error?.code === 800
-      ? <div className="container" style={{textAlign: 'center'}}>
-        <span className="logo__brand">No data found</span>
-        <p>The artist ID has not returned any information. Try a different one</p>
-        <p>
+    {Object.keys((artistInfo).length > 0) ? <PageLayout>
+      <div className="column is-4">
+        <img src={artistInfo.picture_big} alt={artistInfo.title} className="section__cover"/>
+        <div className="section__meta">
           <Link to="/" className="section__back">&larr; Back to home</Link>
-        </p>
+        </div>
       </div>
-      : <Fragment>
-        <PageLayout>
-          <div className="column is-4">
-            <img src={artistInfo?.picture_big} alt={artistInfo?.title} className="section__cover"/>
-            <div className="section__meta">
-              <Link to="/" className="section__back">&larr; Back to home</Link>
-            </div>
-          </div>
-          <div className="column is-offset-1">
-            <h1 className="section__title">
-              {artistInfo.name}
-            </h1>
-            {tracklist?.data?.length > 0 && <h2 className="section__subtitle">
-              Top songs
-            </h2>}
-            <Tracklist tracks={tracklist?.data} />
-          </div>
-        </PageLayout>
-      </Fragment>
-    }
+      <div className="column is-offset-1">
+        <h1 className="section__title">
+          {artistInfo.name}
+        </h1>
+        {tracklist.total > 0 && <h2 className="section__subtitle">
+          Top songs
+        </h2>}
+        <Tracklist tracks={tracklist.data} />
+      </div>
+    </PageLayout> : <div className="container" style={{textAlign: 'center'}}>
+      <span className="logo__brand">No data found</span>
+      <p>The artist ID has not returned any information. Try a different one</p>
+      <p>
+        <Link to="/" className="section__back">&larr; Back to home</Link>
+      </p>
+    </div>}
     <Playbar />
   </main>
 }
